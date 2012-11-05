4478bee058cae946ddfa8a2ea57ebac79cdf3194
Alex Beal <alexlbeal@gmail.com>
2012-11-05 08:45:39 -0700

    Rewritten with continuations.
    The script has been rewritten into two parts. A "Retriever" which
    fetches the most recent tweet from the queue, and a "Tweeter" which
    takes that tweet and posts it to the main account. This new strategy
    uses PyPy's continulets to suspend computation in the Tweeter, while the
    Retriever is fetching a tweet, and then switching back to the Tweeter.
    
    One new behavior is that the Tweeter will waith 60 minutes after the
    most recent tweet, and when that time is up, it will switch to the
    Retriever which will continuously poll twitter for new tweets. Once it
    gets one, it will switch back, and the Tweeter will immediately post it.

--- butterbird.py
+++ butterbird.py
@@ -1,60 +1,76 @@
 import twitter
 import time
-import sys
 import bbconfig
 
-api = twitter.Api(
-        consumer_secret=bbconfig.consumer_sec,
-        consumer_key=bbconfig.consumer_key,
-        access_token_key=bbconfig.access_key,
-        access_token_secret=bbconfig.access_sec)
+from _continuation import continulet
 
-api.VerifyCredentials()
+def wrap_api(api):
+    def wrap_call(api_call, *args, **kwargs):
+        while api.GetRateLimitStatus()["remaining_hits"] < 10:
+            sleep(10 * 60)
+        return api_call(*args, **kwargs)
+    return wrap_call
 
-def UpateDummy(text):
-    print "Dummy update: " + text
+class Tweeter(object):
+    """This class posts to the main account"""
 
-if bbconfig.test_mode == True:
-    api.PostUpdate = UpateDummy
+    def __init__(self, retreiver, post_interval, test_mode=False):
+        """
+           post_interval = Min number of seconds between updates.
+        """
+        self.retriever = retreiver
+        self.post_interval = post_interval
+        self.test_mode = test_mode
+        self.api = twitter.Api(
+                consumer_secret=bbconfig.consumer_sec,
+                consumer_key=bbconfig.consumer_key,
+                access_token_key=bbconfig.access_key,
+                access_token_secret=bbconfig.access_sec)
+        self.api.VerifyCredentials()
+        self.apiCaller = wrap_api(self.api)
 
-## Ask user what tweet to start with.
-# Get the most recent tweets and print them.
-recent_updates = api.GetUserTimeline(bbconfig.queue_username)
-for num, update in enumerate(recent_updates[:-1]):
-    print "%d. %s [id: %d]" % (num, update.text, update.id)
-# Ask the user which tweet to start with.
-while True:
-    start_tweet_num = int(raw_input("Start with which tweet: "))
-    if 0 <= start_tweet_num <= len(recent_updates)-1:
-        break;
-    else:
-        print "Invalid selection."
-# Get the id of the tweet *before* the starting tweet b/c we'll use this as
-# the `since_id` when grabbing tweets from the queue timeline.
-last_id = recent_updates[start_tweet_num+1].id
+    def run(self, cont):
+        while True:
+            while not (9 <= time.localtime().tm_hour < 22):
+                time.sleep(60 * 5)
+            tweet = self.retriever.switch()
+            self.post_update(tweet)
+            time.sleep(self.post_interval)
 
-while True:
-    # Make sure it's between 8 am and 10 pm.
-    cur_hour = time.localtime().tm_hour
-    if not (cur_hour >= 8 and cur_hour < 22):
-        print "It's too late to tweet!"
-        time.sleep(60*10)
-        continue
-    # Sleep if we're near our rate limit.
-    if api.GetRateLimitStatus()["remaining_hits"] < 10:
-        print "Rate limiting..."
-        time.sleep(600)
-        continue
-    # Check the buffer stream for new tweets, and post the oldest new tweet.
-    print "Checking..."
-    untweeted = api.GetUserTimeline(screen_name=bbconfig.queue_username, since_id=last_id)
-    if len(untweeted) > 0:
-        to_post = untweeted[-1]
-        api.PostUpdate(to_post.text)
-        last_id = to_post.id
-        print "Tweeting: %s [id: %d]" % (to_post.text, last_id)
-        # Save last tweet id to file in case the script gets killed.
-        with open(".last_tweet_id", 'w') as id_file:
-            id_file.write(str(last_id))
-    # Sleep between checks/posts.
-    time.sleep(bbconfig.check_interval)
+    def post_update(self, tweet):
+        if self.test_mode:
+            print "Test tweet: " + tweet.text
+        else:
+            pass
+            #self.apiCaller(self.api.PostUpdate, tweet.text)
+
+class Retriever(object):
+    """This class retrieves and deletes from the queue account."""
+
+    def __init__(self, retrieve_interval, test_mode=False):
+        self.retrieve_interval = retrieve_interval
+        self.api = twitter.Api(
+                consumer_secret=bbconfig.consumer_sec,
+                consumer_key=bbconfig.consumer_key,
+                access_token_key=bbconfig.ret_access_key,
+                access_token_secret=bbconfig.ret_access_sec)
+        self.api.VerifyCredentials()
+        self.apiCaller = wrap_api(self.api)
+
+    def retrieveTweet(self, cont):
+        while True:
+            timeline = self.getTimeline()
+            while len(timeline) == 0:
+                time.sleep(self.retrieve_interval)
+                timeline = self.getTimeline()
+            tweet = timeline[-1]
+            cont.switch(tweet)
+            #self.apiCaller(self.api.DestroyStatus(tweet.id))
+
+    def getTimeline(self):
+        return self.apiCaller(self.api.GetUserTimeline)
+
+retriever_cont = continulet(Retriever(5*60, True).retrieveTweet)
+tweeter_cont = continulet(Tweeter(retriever_cont, bbconfig.check_interval, True).run)
+
+tweeter_cont.switch()
